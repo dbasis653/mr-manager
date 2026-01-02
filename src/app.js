19636a96c14b92f6c6842b68eb07f53059d5e3b9
Debasish <dbasis653@gmail.com>
2026-01-02 10:56:41 +0530

    setup the project

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+//BASIC EXPRESS CONFIGURATION
+app.use(express.json({ limit: "16kb" }));
+//to SUPPORT JSON
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+//for data coming through url like %$# in some url
+//to SUPPORT IT
+app.use(express.static("public"));
+//to make "public" folder globally visible
+
+//CORS CONFIGURATION
+//where my FONTEND LIES
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    //what kind of HEADER is allowed
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the BETA");
+});
+
+export default app;
