6d74c9b6e9e28d08af0f027ed6df9111f91e5ed9
Debasish <dbasis653@gmail.com>
2026-01-22 18:36:59 +0530

    Task controllers >> getTaskById, createTask, getTasks

---
+++ src/controllers/tasks.controllers.js
@@ -0,0 +1,180 @@
+import { User } from "../models/user.model.js";
+import { Project } from "../models/project.models.js";
+import { Task } from "../models/task.models.js";
+import { Subtask } from "../models/subtask.models.js";
+
+import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { ApiError } from "../utils/api-error.js";
+
+import mongoose from "mongoose";
+import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
+
+const getTaskById = asyncHandler(async (req, res) => {
+  //when some1 req for a task it wants to see:
+  //TASK, SUB-TASKS, ALL USERS IN THAT TASK
+  const { taskId } = req.params;
+  const task = await Task.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(taskId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "assignedTo",
+        foreignField: "_id",
+        as: "assignedTo",
+        pipeline: [
+          {
+            _id: 1,
+            username: 1,
+            fullName: 1,
+            avatar: 1,
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "subtasks",
+        localField: "_id",
+        foreignField: "task",
+        as: "subtasks",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "createdBy",
+              foreignField: "_id",
+              as: "createdBy",
+              pipeline: [
+                {
+                  $project: {
+                    _id: 1,
+                    username: 1,
+                    fullName: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              createdBy: {
+                $arrayElemAt: ["$createdBy", 0],
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        assignedTo: {
+          $arrayElemAt: ["$assignedTo", 0],
+        },
+      },
+    },
+  ]);
+
+  if (!task || task.length === 0) {
+    throw new ApiError(404, "Task not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, task[0], "Task fetched successfully"));
+});
+
+const createTask = asyncHandler(async (req, res) => {
+  const { title, description, assignedTo, assignedBy, status } = req.body;
+  //'project' field will comes from PARAMS
+  const { projectId } = req.params;
+
+  const project = await Project.findById(projectId);
+
+  if (!project) {
+    throw new ApiError(404, "Project Not found");
+  }
+
+  const files = req.files || [];
+
+  const attachment = files.map((file) => {
+    return {
+      //It stores a full HTTP URL string that points to where the image can be accessed from the browser.
+      url: `${process.env.SERVER_URL}/images/${file.filename}`,
+      mimetype: file.mimetype,
+      size: file.size,
+    };
+  });
+
+  const task = await Task.create({
+    title,
+    description,
+    project: new mongoose.Types.ObjectId(projectId),
+    assignedTo: assignedTo
+      ? new mongoose.Types.ObjectId(assignedTo)
+      : undefined,
+    status,
+    assignedBy: new mongoose.Types.ObjectId(req.user._id),
+    attachment,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, task, "Task created successfully"));
+});
+
+const getTasks = asyncHandler(async (req, res) => {
+  const { projectId } = req.params;
+
+  const project = await Project.findById(projectId);
+
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
+
+  const tasks = await Task.find({
+    project: new mongoose.Types.ObjectId(projectId),
+  }).populate("assignedTo", "avatar username fullName");
+  //populate("assignedTo", "avatar username fullName")
+  //display "assignedTo" from task.models >> In the assignedTo, display "avatar username fullName" these fields which are from User schema
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, tasks, "Task fetched successfully"));
+});
+
+const updateTask = asyncHandler(async (req, res) => {
+  //test
+});
+
+const deleteTask = asyncHandler(async (req, res) => {
+  //test
+});
+
+const createSubTask = asyncHandler(async (req, res) => {
+  //test
+});
+
+const updateSubTask = asyncHandler(async (req, res) => {
+  //test
+});
+
+const deleteSubTask = asyncHandler(async (req, res) => {
+  //test
+});
+
+export {
+  getTasks,
+  createTask,
+  getTaskById,
+  updateTask,
+  deleteTask,
+  createSubTask,
+  updateSubTask,
+  deleteSubTask,
+};
